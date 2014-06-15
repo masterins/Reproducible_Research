b2cddc21978d2258ffe2afdba9b0a5ea9f8d1577
masterins <masterins@hotmail.com>
2014-06-15 15:56:45 -0500

    load new datas

--- Peer_Assessment_1/PA1_template.R
+++ Peer_Assessment_1/PA1_template.R
@@ -1,4 +1,4 @@
-#0
+
 library(ggplot2)
 library(chron)
 library(scales)
@@ -16,19 +16,17 @@ daily_steps<-tapply(files$steps,files$date,sum,na.rm=TRUE)
 
 # Get a graphic with the mean, median 
 dsf<-data.frame(daily_steps)
-mn<-mean(daily_steps)
-md<-median(daily_steps)
-mm<-as.data.frame(c(mn,md))
-names(mm)<-"par"
-mm$cat<-factor(c("mean","median")) #for the 2 vertical lines
-dsf$Vertical_Lines<-factor(rep("1",61)) #artificial factor needed to make a legend
-
+final_mean<-mean(daily_steps)
+final_median<-median(daily_steps)
+final_result<-as.data.frame(c(final_mean,final_median))
+names(final_result)<-"par"
+final_result$cat<-factor(c("mean","median"))
 # with the ggplot2 utility create a graphic histogram
-g<-ggplot(dsf,aes(x=daily_steps,linetype=Vertical_Lines))
-g+geom_histogram(fill="orange",colour="darkblue",binwidth=3000)+
+g<-ggplot(dsf,aes(x=daily_steps))
+g+geom_histogram(fill="blue",colour="darkblue",binwidth=3000)+
   labs(title = "Daily Steps")+
-  geom_vline(data=mm,aes(xintercept=par,linetype=cat))+
-  scale_linetype_discrete(breaks=c("mean","median")) #removes artificial factor from legend
-
+  scale_linetype_discrete(breaks=c("mean","median")) 
 #save the new graphic and call this new graph is pd1steps
-ggsave(filename="p1dsteps.png",width=5,height=4)
+ggsave(filename="daily_steps.png",width=5,height=4)
+
+
